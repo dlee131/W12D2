80ad30de688eb62ff0831c78518adb0873ed1645
Daniel Lee <dlee13195@gmail.com>
2023-01-31 15:15:18 -0500

    phase 1 backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::API
     private
 
     def snake_case_params
-        params.deep_transform_keys!(&:undescore)
+        params.deep_transform_keys!(&:underscore)
     end
 end
